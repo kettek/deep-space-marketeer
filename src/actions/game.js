447bef0c36e24c22b15bfd434e16468454ca7b9b
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-02-06 15:45:43 -0800

    Implement save slot list/delete/create
    For some reason we're creating 0 byte files, however, even though we're
    properly creating the file entry, creating a file writer, and writing
    the json string in a Blob... uncertain what's incorrect, will have to
    double-check documentation of the File API and related.

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,32 +1,62 @@
 export const START_GAME = 'START_GAME'
 
-export function startGame(saveSlot) {
+export function startGame(saveName) {
   return function(dispatch, getState) {
     let state = getState()
     if (state.game.inGame) {
       return
     }
-    console.log('saveslot: ', saveSlot)
     // TODO: ... load file into nativestorage?
     window.resolveLocalFileSystemURL(cordova.file.dataDirectory, fs => {
-      let reader = fs.createReader()
-      reader.readEntries(entries => {
-        console.log('files', entries)
+      let saveFile = ''
+      if (saveName === '') {
+        saveFile = Date.now()
+      } else {
+        saveFile = saveName
+      }
+      // Get our save file
+      fs.getFile(`saves/${saveFile}.json`, {create: true}, fileEntry => {
+        if (saveName === '') { // new save
+          fileEntry.createWriter(fw => {
+            fw.onwriteend = () => {
+              console.log('Created new save file')
+            }
+            fw.onerror = err => {
+              console.log('createSave', err)
+            }
+            let json = JSON.stringify({
+              Player: "test",
+            })
+            let data = new Blob([json], {type: "application/json"})
+            fw.write(data)
+          })
+        } else { // load old save
+          fileEntry.file(file => {
+            console.log(file)
+            let fr = new FileReader()
+            fr.onloadend = () => {
+              console.log('save data', fr.result)
+            }
+            fr.onload = e => {
+              console.log(e)
+              console.log('save data', fr.result)
+            }
+
+            fr.readAsText(file)
+          }, err => {
+            console.log('err on fileread', err)
+          })
+        }
       }, err => {
-        console.log('err', err)
+        console.log("err on file", err)
       })
-      /*fs.getFile(`slot${saveSlot}.json`, {create: false}, file => {
-        console.log("got the file", file);
-      }, err => {
-        console.log("err on file", file)
-      })*/
     }, err => {
       console.log('main dir err', err)
     })
     // cordova.file.dataDirectory
-    return dispatch({
+    /*return dispatch({
       type: START_GAME,
       saveSlot
-    })
+    })*/
   }
 }
