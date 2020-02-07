482f17ee2aee2dbb156d0600a803d8c79ca7ae3f
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-02-07 12:24:16 -0800

    Fix save file creation/loading logic

--- src/actions/game.js
+++ src/actions/game.js
@@ -8,47 +8,46 @@ export function startGame(saveName) {
     }
     // TODO: ... load file into nativestorage?
     window.resolveLocalFileSystemURL(cordova.file.dataDirectory, fs => {
-      let saveFile = ''
       if (saveName === '') {
-        saveFile = Date.now()
-      } else {
-        saveFile = saveName
+        saveName = Date.now()
       }
-      // Get our save file
-      fs.getFile(`saves/${saveFile}.json`, {create: true}, fileEntry => {
-        if (saveName === '') { // new save
+      // Load if exists or create if not
+      fs.getFile(`saves/${saveName}.json`, {create: false}, fileEntry => {
+          fileEntry.file(file => {
+            let fr = new FileReader()
+            fr.onloadend = () => {
+              let profile = JSON.parse(fr.result)
+              console.log(profile)
+              // TODO: dispatch START_GAME with loaded save profile in fr.result
+            }
+            fr.onerror = err => {
+              console.error('reading save', err)
+            }
+            fr.readAsText(file)
+          }, err => {
+            console.error('loading save', err)
+          })
+      }, err => {
+        if (err.code !== FileError.NOT_FOUND_ERR) {
+          console.error('checking save', err)
+          return
+        }
+        fs.getFile(`saves/${saveName}.json`, {create: true}, fileEntry => {
           fileEntry.createWriter(fw => {
-            fw.onwriteend = () => {
-              console.log('Created new save file')
+            fw.onwriteend = (e) => {
+              dispatch(startGame(saveName))
             }
             fw.onerror = err => {
-              console.log('createSave', err)
+              console.error('creating save', err)
             }
+            // TODO: Perhaps load from a factory func like "createPlayer()"
             let json = JSON.stringify({
               Player: "test",
             })
             let data = new Blob([json], {type: "application/json"})
             fw.write(data)
           })
-        } else { // load old save
-          fileEntry.file(file => {
-            console.log(file)
-            let fr = new FileReader()
-            fr.onloadend = () => {
-              console.log('save data', fr.result)
-            }
-            fr.onload = e => {
-              console.log(e)
-              console.log('save data', fr.result)
-            }
-
-            fr.readAsText(file)
-          }, err => {
-            console.log('err on fileread', err)
-          })
-        }
-      }, err => {
-        console.log("err on file", err)
+        })
       })
     }, err => {
       console.log('main dir err', err)
