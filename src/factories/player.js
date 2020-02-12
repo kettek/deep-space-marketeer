fb6a4d882dd254673eec23377855a4403a3b3bf5
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-02-12 12:39:24 -0800

    Add prefix and suffix to player factory

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,7 +1,11 @@
 const createPlayer = ({
+  userPrefix = 'Captain',
   userName = 'Luc',
+  userSuffix = '',
 } = {}) => ({
+  userPrefix,
   userName,
+  userSuffix,
 })
 
 export default createPlayer
